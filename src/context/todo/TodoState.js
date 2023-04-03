846d5ef603d9ce6102dca8083af56cb0bf460f3d
Boris-Maslenov <ya.maslenov@ya.ru>
2023-04-03 11:12:46 +0300

    refactoring fetch

--- src/context/todo/TodoState.js
+++ src/context/todo/TodoState.js
@@ -1,25 +1,23 @@
 import { useReducer, useContext, useCallback } from 'react';
+import { Http } from '../../http';
 import { Vibration, Alert } from 'react-native'
 import { TodoContext } from './todoContext';
 import { todoReducer } from './todoReducer';
 import { ScreenContext } from '../screen/screenContext';
 const DATA_BASE_URL = 'https://react-native-todos-54e7b-default-rtdb.europe-west1.firebasedatabase.app/todos.json';
 const initialState = {
-    todos: [{id: '1', title: 'Выучить React Native'}],
+    todos: [],
 };
 export const TodoState = ({children}) => {
     const {changeScreen} = useContext(ScreenContext);
     const [state, dispatch] = useReducer(todoReducer, initialState);
     const addTodo =  async title =>{
-      const response = await fetch(DATA_BASE_URL,
-          {
-            method: 'POST',
-            headers: {'Content-type': 'application/json'},
-            body: JSON.stringify({title}),
+          try {
+            const data = await Http.post(DATA_BASE_URL, {title});
+            dispatch({type: 'ADD_TODO', payload: {id: data.name, title}});
+          } catch(e) {
+            showError(e.message); 
           }
-      );
-      const data = await response.json();
-      dispatch({type: 'ADD_TODO', payload: {id: data.name, title}});
     };
     const removeTodo = id => {
        const todo = state.todos.find(t => t.id  === id);
@@ -29,13 +27,7 @@ export const TodoState = ({children}) => {
            text: 'Удалить',
            onPress: async () => {
                 try {
-                        const response = await fetch(`https://react-native-todos-54e7b-default-rtdb.europe-west1.firebasedatabase.app/todos/${id}.json`,
-                                                {
-                                                  method: 'DELETE',
-                                                  headers: {'Content-Type' : 'application/json'},
-                                                }
-                                              );
-                        const data = await response.json();
+                      await Http.delete(`https://react-native-todos-54e7b-default-rtdb.europe-west1.firebasedatabase.app/todos/${id}.json`);
                         changeScreen(null);
                         dispatch({type: 'REMOVE_TODO', payload: id});
                 } catch(e) {
@@ -52,11 +44,8 @@ export const TodoState = ({children}) => {
         try {
           clearError();
           showLoader();
-          const response = await fetch(DATA_BASE_URL);
-          const data = await response.json();
-          console.log('Fetch Data: ',data);
-          const todos = Object.entries(data).map(([id, title]) => ({id, ...title}));
-          console.log('todos: ',todos);
+          const response = await Http.get(DATA_BASE_URL);
+          const todos = Object.entries(response).map(([id, title]) => ({id, ...title}));
           dispatch({type: 'FETCH_TODOS', payload: todos});
         } catch(e) {
            showError(e.message);
@@ -68,15 +57,7 @@ export const TodoState = ({children}) => {
 
     const updateTodo = async (id, title) => {
       try {
-        const response = await fetch(`https://react-native-todos-54e7b-default-rtdb.europe-west1.firebasedatabase.app/todos/${id}.json`,
-                  {
-                    method: 'PATCH',
-                    headers: {'Content-Type' : 'application/json'},
-                    body: JSON.stringify({title})
-                  }
-      );
-      const data = await response.json();
-      console.log(data)
+      await Http.patch(`https://react-native-todos-54e7b-default-rtdb.europe-west1.firebasedatabase.app/todos/${id}.json`,  {title});
         dispatch({type: 'UPDATE_TODO', payload: {id, title}});
       } catch(e) {
         showError(e.message);
